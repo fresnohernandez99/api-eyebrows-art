de8b60013ecbe2bc9455e2980d62971cf673af5e
Eddy Yoel Fresno Hdz <fresnohernandez99@gmail.com>
2022-01-15 00:19:33 -0500

    Traying my best

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix("api");
 
 	await app.listen(process.env.PORT || 3000);
